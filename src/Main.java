29d346f0313d23ad18732afca2b3266ed22b49e0
TryniakIgor <94488222+TryniakIgor@users.noreply.github.com>
2023-03-22 19:34:14 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,7,23,76,4,8,};
+        bubbleSort(arr);
+        for (int item: arr) {
+            System.out.println(item);
+        }
+    }
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
